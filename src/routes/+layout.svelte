08f351678b41a99210b8cb60a01f24b490236abd
suvrowebthree <suvro.ghosh.contact@gmail.com>
2023-05-27 16:55:57 +0530

    v1

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+   import "@picocss/pico";
+   
+  </script>
+  <main class="container">
+    <article>
+      <header> <nav>
+        <nav>
+          <ul>
+            <li><strong>Pharos AI</strong></li>
+          </ul>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+       </header>
+      <slot/>
+      <footer>Pharos AI<progress></progress></footer>
+    </article>
+   
+   
+  </main>
+  
+ 
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<main class="container">
+  <h1>Welcome to our AI Consultancy!</h1>
+  <p>Content goes here...</p>
+</main>
+
+  
