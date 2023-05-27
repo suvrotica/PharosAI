e0e0c3fdcf2f5a14313c3e9cef9e17d06563fd17
suvrowebthree <suvro.ghosh.contact@gmail.com>
2023-05-27 17:30:17 +0530

    v5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,29 @@
-<script lang="ts">
-   import "@picocss/pico";
-   
-  </script>
+<script context="module" lang="ts">
+  import { onMount } from 'svelte';
+  import "@picocss/pico";
+  let fontLoaded = false;
+
+  onMount(async () => {
+    const fontLink = document.createElement("link");
+    fontLink.href = "https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap";
+    fontLink.rel = "stylesheet";
+    document.head.appendChild(fontLink);
+    fontLoaded = true;
+  });
+</script>
+
+<svelte:head>
+  <style>
+    body {
+      font-family: 'Roboto', sans-serif;
+    }
+  </style>
+</svelte:head>
+
+
+
+
+
   <main class="container">
     <article>
       <header> <nav>
