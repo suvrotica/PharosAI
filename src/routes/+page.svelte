64d3f921178a5502fa85cb0ddbc125cc03f101d8
suvrowebthree <suvro.ghosh.contact@gmail.com>
2023-05-27 17:53:17 +0530

    v6

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,30 @@
 <main class="container">
   <h1>Welcome to PharosAI!</h1>
-  <p>Pharos means lighthouse in Greek. We envision a future where artificial intelligence is the beacon that guides businesses across all industries towards unprecedented innovation and growth. Our vision is to be the lighthouse in this transformative journey, providing bespoke AI strategies, unparalleled expertise, and continuous support to businesses around the globe.</p>
-  <p>
-    We aspire to be the leading authority on AI integration and trailblazers in AI advancements, consistently pushing the frontier of what is possible. Our goal is to steer our clients through the complexities of the AI landscape, demystifying their path towards AI adoption and helping them unlock unparalleled potential.
-  </p>
-  <p>
-    Through our services, we aim to illuminate a future where AI not only elevates business performance and profitability, but also advocates ethical, inclusive, and sustainable practices. We are committed to guiding a world where businesses harness the power of AI responsibly for the betterment of their operations, their customers, and society at large.
-  </p>
-  <p>In the long term, we envision ourselves as the architects of a new business era, where AI is an integral pillar, not just an additional benefit. We foresee a future where PharosAI, along with our partners and clients, lead the charge in an AI-driven global economy, sculpting the future of business, technology, and society.</p>
+  <div class="content-grid">
+    <img src=".\images\lh.jpeg" alt="lighthouse" class="content-image">
+    <div class="content-text">
+      <p>Pharos means lighthouse in Greek. We envision a future where artificial intelligence is the beacon that guides businesses across all industries towards unprecedented innovation and growth. Our vision is to be the lighthouse in this transformative journey, providing bespoke AI strategies, unparalleled expertise, and continuous support to businesses around the globe.</p>
+      <p>We aspire to be the leading authority on AI integration and trailblazers in AI advancements, consistently pushing the frontier of what is possible. Our goal is to steer our clients through the complexities of the AI landscape, demystifying their path towards AI adoption and helping them unlock unparalleled potential.</p>
+      <p>Through our services, we aim to illuminate a future where AI not only elevates business performance and profitability, but also advocates ethical, inclusive, and sustainable practices. We are committed to guiding a world where businesses harness the power of AI responsibly for the betterment of their operations, their customers, and society at large.</p>
+      <p>In the long term, we envision ourselves as the architects of a new business era, where AI is an integral pillar, not just an additional benefit. We foresee a future where PharosAI, along with our partners and clients, lead the charge in an AI-driven global economy, sculpting the future of business, technology, and society.</p>
+    </div>
+  </div>
 </main>
+<style>
+  .content-grid {
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  gap: 20px;
+}
 
-  
+.content-image {
+  width: 100%;
+  height: auto;
+}
+
+.content-text {
+  /* This will align the text content to the start (top) of its grid area */
+  align-self: start;
+}
+
+</style>
