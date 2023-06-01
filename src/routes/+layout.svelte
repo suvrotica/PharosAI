fd3146a0b81b584035713d3a2f8c54900a5da509
suvrowebthree <suvro.ghosh.contact@gmail.com>
2023-06-01 06:45:51 +0530

    v13

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script context="module" lang="ts">
+    import { onMount } from 'svelte';
+    import "@picocss/pico";
+    let fontLoaded = false;
+  
+    onMount(async () => {
+      const fontLink = document.createElement("link");
+      fontLink.href = "https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap";
+      fontLink.rel = "stylesheet";
+      document.head.appendChild(fontLink);
+      fontLoaded = true;
+    });
+  </script>
+  
+  <svelte:head>
+    <style>
+      body {
+        font-family: 'Roboto', sans-serif;
+      }
+    </style>
+  </svelte:head>
+
+  <main class="container">
+    <article>
+        <header>Header</header>
+       <slot/>
+        <footer>Footer</footer>
+      </article>
+  </main>
+  
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<main class="container">
+
+  <h1>Welcome to PharosAI!</h1>
+    <div class="grid">
+      <article>Pharos means lighthouse in Greek. We envision a future with us as the beacon that guides businesses across all industries towards unprecedented innovation and growth. Our vision in this transformative journey, providing bespoke AI strategies, unparalleled expertise, and continuous support to businesses around the globe. We aspire to be the leading authority on AI integration and trailblazers in AI advancements, consistently pushing the frontier of what is possible. Our goal is to steer our clients through the complexities of the AI landscape, demystifying their path towards AI adoption and helping them unlock unparalleled potential.</article>
+
+     
+    <article>Through our services, we aim to illuminate a future where AI not only elevates business performance and profitability, but also advocates ethical, inclusive, and sustainable practices. We are committed to guiding a world where businesses harness the power of AI responsibly for the betterment of their operations, their customers, and society at large. In the long term, we envision ourselves as the architects of a new business era, where AI is an integral pillar. We foresee a future where PharosAI, along with our partners and clients, lead the charge in an AI-driven global economy, sculpting the future of business, technology, and society.</article>
+   
+  </div>
+
+  <div class="grid">
+  
+  <img src=".\images\img1.png" alt="sequence" class="content-image">
+  <section><br></section>
+  <img src=".\images\img2.png" alt="usecase" class="content-image">
+
+  </div>
+ 
+
+</main>
